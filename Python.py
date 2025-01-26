15895ca10a6a28d96162bcfa19d14851768d4f2a
Your Name <you@example.com>
2025-01-26 12:46:14 -0600

    Week 1

--- Python.py
+++ Python.py
@@ -1,1 +1,6 @@
-its my first class for project capstone.
+# starting a new class under professor Michael's guidance! 
+
+def congratulate(name):
+    print(f"Congratulations for starting a Software Systms Capstone Prjct class, {name}!")
+
+congratulate('Sumayyah')
